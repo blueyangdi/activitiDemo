3ed148f51a1849a153ea1716f734709e7987ce4d
blueyangdi <314346303@qq.com>
2017-01-16 11:21:49 +0800

    修改测试1

--- src/main/java/com/activiti/didi/controller/TestController.java
+++ src/main/java/com/activiti/didi/controller/TestController.java
@@ -14,7 +14,7 @@ public class TestController {
 	@Autowired
 	UserInfoService userInfoService;
 	
-	@RequestMapping("test")
+	@RequestMapping("test2")
 	@ResponseBody
 	public String test() {
 		LogUtil.getLoger().CommonLog().info("测试日志");
