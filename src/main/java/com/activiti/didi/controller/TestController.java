b08fd957910e2f1f9160b0501f19c6b1703980fa
blueyangdi <blueyangdi@163.com>
2017-01-17 16:38:41 +0800

    提交

--- src/main/java/com/activiti/didi/controller/TestController.java
+++ src/main/java/com/activiti/didi/controller/TestController.java
@@ -1,5 +1,9 @@
 package com.activiti.didi.controller;
 
+import java.util.List;
+
+import org.activiti.engine.TaskService;
+import org.activiti.engine.task.Task;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,11 +22,25 @@ public class TestController {
 	@Autowired
 	private MyTaskService myTaskService;
 	
+	@Autowired
+	private TaskService taskService;
+	
 	@RequestMapping("test")
 	@ResponseBody
 	public String test() {
 		LogUtil.getLoger().CommonLog().info("测试日志");
-		String id = myTaskService.startProcessInstanceByKey("myProcess");
+		String name = "杨迪个人";
+		String id = myTaskService.startProcessInstanceByKey("helloProcess");
+		List<Task> taskList = taskService.createTaskQuery().taskAssignee(name).list();
+		for (int i = 0; i < taskList.size(); i++) {
+			System.out.println(taskList.get(i).getAssignee());
+			System.out.println(taskList.get(i).getDescription());
+			System.out.println(taskList.get(i).getName());
+			System.out.println(taskList.get(i).getOwner());
+			System.out.println(taskList.get(i).getId());
+		}
+		taskService.complete(taskList.get(0).getId());
+		
 		LogUtil.getLoger().CommonLog().info(id);
 		return userInfoService.getUserInfo().getName();
 	}
